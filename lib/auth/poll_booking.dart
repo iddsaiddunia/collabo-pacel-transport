629e3228e0d7ea71444ce38f2bfc3f401f37c6fc
iddsaiddunia <idrisdunia@gmail.com>
2024-05-03 14:07:03 +0300

    poll_booking minor updates

--- lib/auth/poll_booking.dart
+++ lib/auth/poll_booking.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:pacel_trans_app/color_themes.dart';
 
-
 final primaryColor = new ColorTheme();
+
 class PollBookingPage extends StatefulWidget {
   const PollBookingPage({super.key});
 
@@ -34,74 +34,182 @@ class _PollBookingPageState extends State<PollBookingPage> {
     "other",
   ];
 
-  _selectPackageType(index){
+  _selectPackageType(index) {
     setState(() {
       _selectedIndex = index;
     });
   }
 
-  _selectPackageSize(index){
+  _selectPackageSize(index) {
     setState(() {
       _selectedPackageSizeIndex = index;
     });
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-      ),
+      appBar: AppBar(),
       body: Padding(
         padding: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 15),
-        child: ListView(children: [
-          Text("Package details",style: TextStyle(fontSize: 19,fontWeight: FontWeight.w600),),
-          Text("what are you sending?",style: TextStyle(fontSize: 11,fontWeight: FontWeight.w600),),
-          SizedBox(height: 20.0,),
-          Container(
-            width: double.infinity,
-            height: 60,
-            child: ListView.builder(
-              scrollDirection: Axis.horizontal,
-              itemCount:  packageTypeList.length,
-              itemBuilder: (context, index){
-              return PackageType(title: packageTypeList[index], ontap: (){
-                _selectPackageType(index);
-              }, index: index,selectedIndex: _selectedIndex,);
-            },),
-          ),
-          SizedBox(height: 25,),
-          Text("Package size",style: TextStyle(fontSize: 19,fontWeight: FontWeight.w600),),
-          SizedBox(height: 10,),
-          Container(
-            width: double.infinity,
-            height: 120,
-            child: ListView.builder(
-              scrollDirection: Axis.horizontal,
-              itemCount:  packageSize.length,
-              itemBuilder: (context, index){
-                return PackageSize(title: packageSize[index], ontap: (){
-                  _selectPackageSize(index);
-                }, index: index,selectedIndex: _selectedPackageSizeIndex,);
-              },),
-          ),
-          SizedBox(height: 30.0,),
-          Row(children: [
-            Checkbox(
-              value: this._isChecked,
-              onChanged: _onCheckboxChanged
+        child: ListView(
+          children: [
+            Text(
+              "Package details",
+              style: TextStyle(fontSize: 19, fontWeight: FontWeight.w600),
             ),
-            Text("Breakable items")
-          ],
-          ),
-          Row(children: [
-            Checkbox(
-                value: this._isChecked,
-                onChanged: _onCheckboxChanged
+            Text(
+              "what are you sending?",
+              style: TextStyle(fontSize: 11, fontWeight: FontWeight.w600),
             ),
-            Text("No prohibited items")
+            SizedBox(
+              height: 20.0,
+            ),
+            Container(
+              width: double.infinity,
+              height: 60,
+              child: ListView.builder(
+                scrollDirection: Axis.horizontal,
+                itemCount: packageTypeList.length,
+                itemBuilder: (context, index) {
+                  return PackageType(
+                    title: packageTypeList[index],
+                    ontap: () {
+                      _selectPackageType(index);
+                    },
+                    index: index,
+                    selectedIndex: _selectedIndex,
+                  );
+                },
+              ),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Text(
+              "Package size",
+              style: TextStyle(fontSize: 19, fontWeight: FontWeight.w600),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Container(
+              width: double.infinity,
+              height: 120,
+              child: ListView.builder(
+                scrollDirection: Axis.horizontal,
+                itemCount: packageSize.length,
+                itemBuilder: (context, index) {
+                  return PackageSize(
+                    title: packageSize[index],
+                    ontap: () {
+                      _selectPackageSize(index);
+                    },
+                    index: index,
+                    selectedIndex: _selectedPackageSizeIndex,
+                  );
+                },
+              ),
+            ),
+            SizedBox(
+              height: 25.0,
+            ),
+            Row(
+              children: [
+                Checkbox(value: this._isChecked, onChanged: _onCheckboxChanged),
+                Text("Breakable items")
+              ],
+            ),
+            Row(
+              children: [
+                Checkbox(value: this._isChecked, onChanged: _onCheckboxChanged),
+                Text("No prohibited items")
+              ],
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Text(
+              "Payment details",
+              style: TextStyle(fontSize: 19, fontWeight: FontWeight.w600),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Container(
+              width: double.infinity,
+              height: 130.0,
+              padding: EdgeInsets.all(8),
+              decoration: BoxDecoration(color: Colors.white),
+              child: Column(
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        "Estimeted price",
+                        style: TextStyle(),
+                      ),
+                      Row(children: [
+                        Text(
+                          "30,000 Tsh",
+                          style: TextStyle(
+                              fontWeight: FontWeight.w600,
+                              color: color.primaryColor),
+                        ),
+                        IconButton(
+                          onPressed: () {},
+                          icon: Icon(
+                            Icons.arrow_forward_ios,
+                            color: Colors.black26,
+                          ),
+                          iconSize: 20,
+                        )
+                      ])
+                    ],
+                  ),
+                  Divider(),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        "Payment method",
+                        style: TextStyle(),
+                      ),
+                      Row(
+                        children: [
+                          Text(
+                            "Cash",
+                            style: TextStyle(fontWeight: FontWeight.w600),
+                          ),
+                          IconButton(
+                            onPressed: () {},
+                            icon: Icon(
+                              Icons.arrow_forward_ios,
+                              color: Colors.black26,
+                            ),
+                            iconSize: 20,
+                          )
+                        ],
+                      )
+                    ],
+                  )
+                ],
+              ),
+            ),
+            SizedBox(height: 20,),
+            MaterialButton(
+              elevation: 0,
+              height: 55,
+              color: color.primaryColor,
+              onPressed: () {},
+              child: Text(
+                "Place order",
+                style: TextStyle(color: Colors.white,fontWeight: FontWeight.bold),
+              ),
+            ),
           ],
-          ),
-
-        ],),
+        ),
       ),
     );
   }
@@ -112,12 +220,12 @@ class PackageType extends StatelessWidget {
   final Function()? ontap;
   final int index;
   final int selectedIndex;
-  const PackageType({super.key,
-    required this.title,
-    required this.ontap,
-    required this.index,
-    required this.selectedIndex
-  });
+  const PackageType(
+      {super.key,
+      required this.title,
+      required this.ontap,
+      required this.index,
+      required this.selectedIndex});
 
   @override
   Widget build(BuildContext context) {
@@ -129,18 +237,24 @@ class PackageType extends StatelessWidget {
         // width: 130,
         height: 60,
         decoration: BoxDecoration(
-          color: Colors.white,
-            border: (index==selectedIndex)?Border.all(width: 2, color:  Color.fromRGBO(47, 66, 96, 1.0)):Border.all(width: 1, color:  Colors.black12),
-          borderRadius: BorderRadius.all(Radius.circular(10))
-        ),
+            color: Colors.white,
+            border: (index == selectedIndex)
+                ? Border.all(width: 2, color: Color.fromRGBO(47, 66, 96, 1.0))
+                : Border.all(width: 1, color: Colors.black12),
+            borderRadius: BorderRadius.all(Radius.circular(10))),
         child: Row(
           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [Icon(Icons.book),
+          children: [
+            Icon(Icons.book),
             Padding(
               padding: const EdgeInsets.all(8.0),
-              child: Text(title,style: TextStyle(fontWeight: FontWeight.w600),),
+              child: Text(
+                title,
+                style: TextStyle(fontWeight: FontWeight.w600),
+              ),
             ),
-          ],),
+          ],
+        ),
       ),
     );
   }
@@ -151,13 +265,12 @@ class PackageSize extends StatelessWidget {
   final Function()? ontap;
   final int index;
   final int selectedIndex;
-  const PackageSize({super.key,
-    required this.title,
-    required this.ontap,
-    required this.index,
-    required this.selectedIndex
-  });
-
+  const PackageSize(
+      {super.key,
+      required this.title,
+      required this.ontap,
+      required this.index,
+      required this.selectedIndex});
 
   @override
   Widget build(BuildContext context) {
@@ -166,21 +279,24 @@ class PackageSize extends StatelessWidget {
       child: Column(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
-        Container(
-          margin: EdgeInsets.symmetric(horizontal: 5),
-          padding: EdgeInsets.all(18),
-          width: 95,
-          height: 95,
-          decoration: BoxDecoration(
-              color: Colors.white,
-              border: (index==selectedIndex)?Border.all(width: 2, color:  Color.fromRGBO(47, 66, 96, 1.0)):Border.all(width: 1, color:  Colors.black12),
-              borderRadius: BorderRadius.all(Radius.circular(10))
+          Container(
+            margin: EdgeInsets.symmetric(horizontal: 5),
+            padding: EdgeInsets.all(18),
+            width: 95,
+            height: 95,
+            decoration: BoxDecoration(
+                color: Colors.white,
+                border: (index == selectedIndex)
+                    ? Border.all(
+                        width: 2, color: Color.fromRGBO(47, 66, 96, 1.0))
+                    : Border.all(width: 1, color: Colors.black12),
+                borderRadius: BorderRadius.all(Radius.circular(10))),
+            child: Image.asset('assets/img/box.png'),
           ),
-          child: Image.asset('assets/img/box.png'),
-        ),
-        Text(title,style: TextStyle(fontWeight: FontWeight.w600, fontSize: 12))
-      ],)
+          Text(title,
+              style: TextStyle(fontWeight: FontWeight.w600, fontSize: 12))
+        ],
+      ),
     );
   }
 }
-
